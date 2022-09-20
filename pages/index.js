dfadb65c0d0b032ddce6cb56a3e8e5299ff2002d
juanflarrotta <juanlarrotta@gmail.com>
2022-09-19 20:15:27 -0500

    creacion de proyecto base

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Juan F. Larrotta Bernal</title>
+        <meta name="description" content="Portafolio de Juan F. Larrotta Bernal" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  )
+}
