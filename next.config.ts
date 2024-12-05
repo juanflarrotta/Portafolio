498b6e4688df10393ab03446427033deeebe91a0
juanflarrotta <juanlarrotta@habi.co>
2024-12-05 16:18:07 -0500

    ajuste build

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  distDir: 'dist',
+  // distDir: 'build',
+  // trailingSlash: true,
+    assetPrefix: 'https://juanflarrotta.github.io/Portafolio/'
+    // staticPageGenerationTimeout: 1000,
+    // reactStrictMode: false
 };
 
 export default nextConfig;
